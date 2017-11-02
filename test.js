bd4621c6206c6930cbafe4522e0a187000164369
Joseph Frazier <1212jtraceur@gmail.com>
2017-11-01 22:18:08 -0400

    Use prettier to lint/fix code style

--- test.js
+++ test.js
@@ -1,36 +1,45 @@
-const assert = require('assert');
+const assert = require("assert");
 
-const editorconfigToPrettier = require('./');
+const editorconfigToPrettier = require("./");
 
-assert.deepStrictEqual(editorconfigToPrettier({
-  indent_style: 'tab',
-  tab_width: 8,
-  indent_size: 2,
-  max_line_length: 100
-}), {
-  useTabs: true,
-  tabWidth: 8,
-  printWidth: 100
-})
+assert.deepStrictEqual(
+  editorconfigToPrettier({
+    indent_style: "tab",
+    tab_width: 8,
+    indent_size: 2,
+    max_line_length: 100
+  }),
+  {
+    useTabs: true,
+    tabWidth: 8,
+    printWidth: 100
+  }
+);
 
-assert.deepStrictEqual(editorconfigToPrettier({
-	indent_style: 'space',
-	tab_width: 8,
-	indent_size: 2,
-	max_line_length: 100
-}), {
-  useTabs: false,
-  tabWidth: 2,
-  printWidth: 100
-})
+assert.deepStrictEqual(
+  editorconfigToPrettier({
+    indent_style: "space",
+    tab_width: 8,
+    indent_size: 2,
+    max_line_length: 100
+  }),
+  {
+    useTabs: false,
+    tabWidth: 2,
+    printWidth: 100
+  }
+);
 
-assert.deepStrictEqual(editorconfigToPrettier({
-  indent_style: 'space',
-  tab_width: 8,
-  indent_size: 8,
-  max_line_length: 100
-}), {
-  useTabs: false,
-  tabWidth: 8,
-  printWidth: 100
-})
+assert.deepStrictEqual(
+  editorconfigToPrettier({
+    indent_style: "space",
+    tab_width: 8,
+    indent_size: 8,
+    max_line_length: 100
+  }),
+  {
+    useTabs: false,
+    tabWidth: 8,
+    printWidth: 100
+  }
+);
